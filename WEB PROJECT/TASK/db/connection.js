404df34a55f73e344ff44d5bb209a9f7a8bbe531
Amrou Al Mfalani <52882257+AfterMath9@users.noreply.github.com>
2025-10-04 19:00:47 +0200

    Delete WEB PROJECT directory

--- WEB PROJECT/TASK/db/connection.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-const sqlite3 = require('sqlite3').verbose();
-
-const dbPath = path.join(__dirname, '..', 'data', 'app.sqlite');
-const db = new sqlite3.Database(dbPath);
-
-module.exports = db;
